5612785a624a6b0622c3d8b90feb780047dc88fc
Apple <apple@Apples-MacBook-Pro-3.local>
2019-06-05 11:19:54 -0500

    version1

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-var userInput;
+// var userInput;
 
-function: search()
-if input="red" {
-  return class="red"
+// function: search()
+// if input="red" {
+//   return class="red"
+// }
+function searchFunction() {
+    var input, filter, flowers, a, i;
+    input = document.getElementByClass('search-bar');
+    filter = input.value.toUpperCase();
+    .flowers = document.getElementById('wrapper');
+    li = flowers.getElementsByTagName('li');
+
+    for(i=0 ; i< li.length; i++){
+        a = li[i].getElementsByClassName('a')[0];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = "";
+        }
+
+        else{
+            li[i].style.display = 'none';
+        }
+    }
 }
