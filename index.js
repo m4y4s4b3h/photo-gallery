dee271ae0f41d29b9ee84d5c859d9af156e97fca
Apple <apple@Apples-MacBook-Pro-3.local>
2019-06-07 16:45:17 -0500

    weekendVersion

--- index.js
+++ index.js
@@ -6,19 +6,26 @@
 // }
 function searchFunction() {
     var input, filter, flowers, a, i;
-    input = document.getElementByClass('search-bar');
+    input = document.getElementsByClassName('search-bar').value;
     filter = input.value.toUpperCase();
-    .flowers = document.getElementById('wrapper');
-    li = flowers.getElementsByTagName('li');
+    flowers = document.getElementByClassName('flowers');
 
-    for(i=0 ; i< li.length; i++){
-        a = li[i].getElementsByClassName('a')[0];
-        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
-            li[i].style.display = "";
-        }
+    for (i = 0; i < x.length; i++) {
+            if (!x[i].innerHTML.toUpperCase().includes(input)) {
+                x[i].style.display="none";
+            }
+            else {
+                x[i].style.display="list-item";
+    // li = flowers.getElementsByClassName('li');
 
-        else{
-            li[i].style.display = 'none';
+    // for(i=0 ; i< li.length; i++){
+    //     a = flowers[i].getElementsByClassName('a')[0];
+    //     if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
+    //         li[i].style.display = "";
+    //     }
+    //
+    //     else{
+    //         li[i].style.display = 'none';
         }
     }
 }
